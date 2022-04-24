ede33fd03fdfd3b184467e22c692cb7c16f2676b
unknown <tinb93273@student.ctu.edu.vn>
2022-04-24 14:18:00 +0700

    all project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+        <a href="/" class="navbar-brand">Website Search Restaurant</a>
+        <div class="mr-auto navbar-nav">
+            <li class="nav-item">
+                <router-link :to="{ name: 'restaurant' }" class="nav-link">
+                    Trang chủ
+                    <i class="fas fa-map"></i>
+                </router-link>
+            </li>
+            <li class="nav-item">
+                <router-link :to="{ name: 'menu' }" class="nav-link">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </router-link>
+            </li>
+        </div>
+    </nav>
+</template>
